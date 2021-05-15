e5ffc3a3f199616dc29fef7df31bca33f0b402a1
keisuke-1-7 <keisuke17.business@gmail.com>
2021-05-15 15:28:41 +0000

    TOP画面の作成

---
+++ ChatBoard/resources/views/posts/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts')
+
+@section('content')
+
+    <div class="container mt-4">
+        <div class="card mt-4">
+            <div class="card-header">
+                    タイトル
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    内容
+                </p>
+            </div>
+            <div class="card-footer">
+                <span class="mr-2">
+                    投稿日時
+                </span>
+            </div>        
+        </div>
+    </div>
+
+
+@endsection('content')
