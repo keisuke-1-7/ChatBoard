9e6f18945fb8bc89c4813a38877e275443dc6035
keisuke-1-7 <keisuke17.business@gmail.com>
2021-05-22 15:05:34 +0000

    ルーティング追加

--- ChatBoard/routes/web.php
+++ ChatBoard/routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 Route::get('/', 'PostsController@index')->name('top');
 
-Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);
